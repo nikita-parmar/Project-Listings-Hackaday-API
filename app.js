1a25b92996655c64c251fb946294a2fe464ddfab
Nikita Parmar <nikitaparmar@Nikitas-MacBook-Pro.local>
2019-04-19 16:27:04 -0700

    Simple Pagination Implemented

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ const projectlistRoute = require('./routes/projectlist')
 app.use(cors());
 app.use('/projectlist', projectlistRoute)
 
+// to render index.html on starting the server
+app.use(express.static("./"));
 module.exports = app;
--- routes/projectlist.js
+++ routes/projectlist.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 router.get('/:page',(req,response,next)=>{
 	var page = req.params.page;
-	console.log(page);
 	var projectlistUrl = "http://api.hackaday.io/v1/projects?api_key=z9By0e11PbHb3dQu&page=" + page;
+	console.log(projectlistUrl);
 		
 	request(projectlistUrl,{ json: true }, (err, res, body) => {
 		response.status(200).json({
