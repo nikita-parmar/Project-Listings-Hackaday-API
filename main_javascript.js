c941bf14a9c0023d7e584bc6af9283609ff72316
Nikita Parmar <nikitaparmar@Nikitas-MacBook-Pro.local>
2019-04-19 21:45:29 -0700

    Styling done

--- main_javascript.js
+++ main_javascript.js
@@ -18,7 +18,7 @@ $(function() {
     $("#pagination").pagination({
         items: total_items,
         itemsOnPage: per_page,
-        cssStyle: 'light-theme',
+        cssStyle: 'compact-theme',
         onPageClick: function(pageNumber, event){
         	$.ajax({
         		url: "http://localhost:4000/projectlist/"+pageNumber, 
@@ -41,26 +41,33 @@ function display_projects(result){
 			html_string += "<div class='card'>";
 			html_string += "<div class='card-data'>";
 			html_string += "<div class='leftdata'>"; 
-			html_string += "<h3 class='name'>"+ project_array[i].name + "</h3>"; 
+			html_string += "<div class='name'><b>"+ project_array[i].name + "</b></div>"; 
 			html_string += "<p class='summary'>"+ project_array[i].summary + "</p>"; 	
-			html_string += "<div class='tooltip'>Owner:" + project_array[i].owner_id;
+			html_string += "<div class='projecturl'><a href=" +  project_array[i].url + ">Go to the Project</a></div>";			
+			
+	html_string += "<div class='tooltip'>Owner:" + project_array[i].owner_id;
 			html_string += "<div class='tooltipload'>Loading...</div>";
 			html_string += "<div class='tooltiptext'></div>";
 			html_string +=	"</div>"; //end of owner tooltip 
-
-			html_string += "<p class='tags'>Tags: </p>";
+		
+			
+			html_string += "<div class='alltags'>";
+			html_string += "<p class='tags'><b>Tags: </b></p>";
 			tag_array = project_array[i].tags;
 			if(tag_array!=null){
 				for(var j=0; j<Math.min(tag_array.length,5) ;j++){
-					html_string += "<p class='tags'>"+ tag_array[j] + ", </p>"; 
+					html_string += "<p class='tags'>"+ tag_array[j] + "</p>"; 
 			 	}
 			}
-
+			html_string += "</div>"; // end of all tags
+			
 			html_string += "</div>"; // div leftdata ends
 
 			html_string += "<div class='rightimg'><img src='" + project_array[i].image_url + "' style='height:100%; width:100%;'></div>";//image div	
 			html_string += "</div>"; // card-data div
 
+			
+		
 			html_string += "<div class='footerdiv footer-element'>"; 
 			html_string += "<p class='footer-element'><i class='fas fa-users'></i> " + project_array[i].followers + " </p>";
 			html_string += "<p class='footer-element'><i class='far fa-comments'></i> " + project_array[i].comments + " </p>";
