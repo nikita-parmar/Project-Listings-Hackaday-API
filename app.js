efd1394822294ec08eb404863840372ab41d6d03
Nikita Parmar <nikitaparmar@Nikitas-MacBook-Pro.local>
2019-04-19 19:23:07 -0700

    User route & owner tooltip

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 
-const projectlistRoute = require('./routes/projectlist')
-
+const projectlistRoute = require('./routes/projectlist');
+const userdetailRoute = require('./routes/userdetail');
 app.use(cors());
-app.use('/projectlist', projectlistRoute)
+app.use('/projectlist', projectlistRoute);
+app.use('/userdetail', userdetailRoute);
 
 // to render index.html on starting the server
 app.use(express.static("./"));
---
+++ routes/userdetail.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+
+router.get('/:id',(req,response,next)=>{
+	var owner_id = req.params.id;
+	var userDetailUrl = "http://api.hackaday.io/v1/users/" + owner_id + "?api_key=z9By0e11PbHb3dQu";
+	
+	request(userDetailUrl,{ json: true }, (err, res, body) => {
+		response.status(200).json({
+			body
+			});
+		});
+	});
+
+module.exports = router;
+
